60e29283e17abb1a637bb4f8ec12e991711d06c1
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-14 09:31:58 +0200

    chat page skeleton

--- client/screens/Test.js
+++ client/screens/Test.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import UIButton from '../components/basic/UIButton'
 import tw from '../utils/config/tailwindConf'
+import Chat from './chat/Chat'
+import ChatList from './chat/ChatList'
 
 const Test = () => {
   return (
     <View style={tw`flex flex-col w-full items-center	justify-center`}>
-      <Text style={tw`text-center font-bold text-xl p-5`}>this is an example:</Text>
-      <UIButton
+      {/* <Text style={tw`text-center font-bold text-xl p-5`}>this is an example:</Text> */}
+      <ChatList/>
+      {/* <UIButton
         padding="5"
         width="4/5"
         color="secondary"
@@ -15,7 +18,7 @@ const Test = () => {
         textColor="red-700"
         textSize="2xl"
         rounded="3xl"
-      />
+      /> */}
     </View>
   )
 }
