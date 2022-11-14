9e309b8a42a33f7f4f5d452104febc07cef4a914
GiladLev <giladlev2000@gmail.com>
2022-11-14 11:23:29 +0200

    fix merge swipper

---
+++ client/screens/Test.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './Home'
+
+const Test = () => {
+  return (
+    <View>
+
+      <Home/>
+    </View>
+  )
+}
+
+export default Test
