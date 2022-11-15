edab7016bf7ace90d6ed75b9fdc5f8cb25296ab9
MATANBENSHABAT <matanbs2000@gmail.com>
2022-11-15 19:16:32 +0200

    login page logix

--- client/screens/authorization/login/Login.js
+++ client/screens/authorization/login/Login.js
@@ -1,11 +1,58 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
+import { Button, Text, TextInput, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Login = () => {
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const login = async () => {
+    await axios
+      .post('http://10.0.2.2:4000/auth/login', {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        // console.log()
+        const storeData = async (value) => {
+          const jsonValue = JSON.stringify(value)
+          try {
+            await AsyncStorage.setItem('data', jsonValue)
+          } catch (e) {
+            console.log(e)
+          }
+        }
+
+        const getData = async () => {
+          try {
+            const jsonValue = await AsyncStorage.getItem('data')
+            jsonValue != null ? JSON.parse(jsonValue) : null
+            console.log(jsonValue)
+          } catch (e) {
+            // error reading value
+            console.log(e)
+          }
+        }
+
+        storeData(res.data)
+        getData()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  const handlePress = () => {
+    console.log(email)
+    console.log(password)
+  }
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput placeholder="Email..." onChangeText={setEmail}></TextInput>
+      <TextInput placeholder="Password..." onChangeText={setPassword}></TextInput>
+      <Button title="Login" onPress={login}></Button>
+    </SafeAreaView>
   )
 }
 
