9746cd41fa8db09b879b345cab0fb920c40d030a
GiladLev <giladlev2000@gmail.com>
2022-11-16 13:49:32 +0200

    chat with two virson

--- client/utils/socket/socket.js
+++ client/utils/socket/socket.js
@@ -1,7 +1,3 @@
-const { Server } = require("socket.io");
-
-const io = new Server({
-  serveClient: false
-});
-
-export default io
+import { io } from "socket.io-client";
+const socket = io.connect("http://localhost:5000");
+export default socket;
