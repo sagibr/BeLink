3ed39f709c1bf7a6ac88a8567a8d0e7fbe79e8d4
GiladLev <giladlev2000@gmail.com>
2022-11-16 14:45:51 +0200

    fix probleam after rebase socket and login

--- client/screens/Test.js
+++ client/screens/Test.js
@@ -1,20 +1,12 @@
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
-import ChatRow from '../components/ChatClone/ChatRow'
 import tw from '../utils/config/tailwindConf'
-import ChatList from './chat/ChatList'
-import Messaging from './chat/Messaging'
-import ChatScreen from './ChatScreen'
+
 const Test = () => {
-  // const socket = io('http://localhost:5000')
 
   return (
     <View style={tw``}>
-      {/* <ChatList/> */}
-      {/* <Messaging/> */}
-      {/* <AvatarChat/> */}
-      {/* <ChatRow/> */}
-      <ChatScreen/>
+      <Text>Test</Text>
     </View>
   )
 }
