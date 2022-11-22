786dd6e22a166c6cac791687dcd1b6d80c42ef93
Sagi Barabi <sbarabi1@gmail.com>
2022-11-22 11:09:12 +0200

    added env port to socket

--- socket/index.js
+++ socket/index.js
@@ -1,45 +1,42 @@
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-const cors = require("cors");
-const PORT = 5000;
-const socketIO = require("socket.io")(http, {
-	cors: {
-		origin: "*",
-	},
-});
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const cors = require('cors')
+const PORT = process.env.port || 5000
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  },
+})
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
-socketIO.on("connection", (socket) => {
-	console.log(` ${socket.id} user just connected!`);
+socketIO.on('connection', (socket) => {
+  console.log(` ${socket.id} user just connected!`)
 
-	socket.on("join_room", (data) => {
-		socket.join(data);
-	  });
-	
-	socket.on("send_message", (data) => {
-		const { room_id, text, user, timestamp } = data;
-		console.log("data", data);
-		const newMessage = {
-			text,
-			user,
-			time: `${timestamp.hour}:${timestamp.mins}`,
-		};
-		console.log("New Message", newMessage);
-		socket.to(room_id).emit("receive_message", newMessage);
+  socket.on('join_room', (data) => {
+    socket.join(data)
+  })
 
-	});
-	socket.on("disconnect", () => {
-		socket.disconnect();
-		console.log("A user disconnected");
-	});
-});
-
-
+  socket.on('send_message', (data) => {
+    const { room_id, text, user, timestamp } = data
+    console.log('data', data)
+    const newMessage = {
+      text,
+      user,
+      time: `${timestamp.hour}:${timestamp.mins}`,
+    }
+    console.log('New Message', newMessage)
+    socket.to(room_id).emit('receive_message', newMessage)
+  })
+  socket.on('disconnect', () => {
+    socket.disconnect()
+    console.log('A user disconnected')
+  })
+})
 
 http.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`)
+})
