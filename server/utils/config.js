2861783b69fb662217808e1267783b91e404d024
Gilad Lev <40337277+GiladLev@users.noreply.github.com>
2022-11-23 17:48:32 +0200

    Update config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -8,10 +8,5 @@ export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
 export const NODE_ENV = process.env.NODE_ENV
 
 export const allowedOrigins = [
-  'https://belink.netlify.app/',
-  'https://belink1.netlify.app/',
-  'http://localhost:3500',
-  'http://localhost:3000',
-  'exp://10.195.25.165:19000',
-  'http://localhost:19006',
+  "https://belink.netlify.app"
 ]
